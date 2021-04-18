a9faeab8c8f31c69beee4ee97464a4f050db4bc7
ErikaRubi <eri.ru27c.a@gmail.com>
2021-04-18 13:37:33 -0500

    Cambio de home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TECPAM
                 </div>
 
                 <div class="links">
