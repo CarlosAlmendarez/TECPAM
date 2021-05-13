798f2d1e1db9292d716845803c5391c39bc878d7
ErikaRubi <eri.ru27c.a@gmail.com>
2021-05-12 23:19:58 -0500

    detalle en app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
                         <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
-                            Cerar sesión
+                            Cerrar sesión
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                             @csrf
